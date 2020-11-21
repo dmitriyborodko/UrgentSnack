896faebb1fa081ad34af32ed7e1ea7da0d4145b3
Dmitrii Borodko <90deem@yandex-team.ru>
2020-11-21 17:43:09 +0300

    added cocoapods

---
+++ UrgentSnack/Pods/Target Support Files/Pods-UrgentSnack-UrgentSnackUITests/Pods-UrgentSnack-UrgentSnackUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UrgentSnack_UrgentSnackUITests : NSObject
+@end
+@implementation PodsDummy_Pods_UrgentSnack_UrgentSnackUITests
+@end
---
+++ UrgentSnack/Pods/Target Support Files/Pods-UrgentSnack/Pods-UrgentSnack-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UrgentSnack : NSObject
+@end
+@implementation PodsDummy_Pods_UrgentSnack
+@end
